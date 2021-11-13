c97cfddabfdb698cfc9eb0246b3ba40afd5b4a0d
Dare Samuel ANYARONKE <dwayne-12@hotmail.fr>
2021-11-13 15:33:58 +0100

    update index.php 2

--- index.php
+++ index.php
@@ -2,85 +2,40 @@
 
 
 use App\Controllers\HomeController;
-
 require_once "vendor/autoload.php";
 
-$test = new HomeController();
-
-
-echo "<pre>";
-
+$app = new HomeController();
 //  on recupere notre URL
 $page = $_GET["page"];
 
+// on casse l'url
+
+$page = explode('/', $page);
 
 // traitement de notre url
 
 try {
     // on traie le cas où l'url est sur index.php ( donc vide )
     if ( empty($_GET["page"]) ) {
-        $test->home();
+        $app->home();
     } else {
-
+        // on va traiter tous les info a partir de index 0
+        switch ($page[0]) {
+            case '/':
+            case 'home':
+                $app->home();
+                break;
+            case 'admin':
+                if ($page[1] === 'dashboard') {
+                    echo "la page d'acceuil de l'administrateur";
+                }
+                if ($page[1] === 'login') {
+                    echo "la page de connexion de l'adminitrateur (lety)";
+                }
+                break;
+        }
     }
 
 } catch (Exception $e) {
     die( $e->getMessage() );
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// on va casser notre url pour savoir si on a plusieur demande
-$page = explode('/', $page);
-var_dump($page);
-
-if (isset($url) && !empty($url)) {
-
-    // on match les url pour afficher les bonnes pages
-    try {
-        switch ($url) {
-            case "/" :
-            case "home":
-            case "acceuil":
-                $test->home();
-                break;
-            case "contact" :
-                echo "page de contact";
-                break;
-            default : throw new Exception("la page n'existe pas");
-        }
-
-    } catch (Exception $e) {
-        die( $e->getMessage() );
-    }
-} else {
-    // afficher la page d'acceuil
-    echo "afficher acceuil ";
 }
