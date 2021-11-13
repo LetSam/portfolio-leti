5f2ebb653f38b0c09863f19cb798e2eecfe5f1fe
ltadjer <94218573+ltadjer@users.noreply.github.com>
2021-11-13 12:51:17 +0100

    page home

--- App/Views/home.php
+++ App/Views/home.php
@@ -1,1 +1,343 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <!-- ICONS -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.0/css/line.css">
+    <!-- SWIPER -->
+    <link rel="stylesheet" href="src/css/swiper-bundle.min.css">
+    <link rel="stylesheet" href="https://use.typekit.net/aav2jsr.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="src/css/variables.css">
+    <link rel="stylesheet" href="src/css/style.css">
+    <title>Portfolio - Liticia TADJER</title>
+    
+</head>
+
+<body>
+    <!-- HEADER -->
+    <header class="main-header">
+        <div class="header-container container">
+                <a href="#" class="header-logo"><img src="src/images/logo.png" alt="mon_logo"></a>
+            <nav class="main-nav">
+                <ul>
+                    <li><a href="#accueil" >Accueil</a></li>
+                    <li><a href="#a-propos">A propos</a></li>
+                    <li><a href="#competences">Compétences</a></li>
+                    <li><a href="#projets">Projets</a></li>
+                    <li> <a href="#contact"> Contact</a></li>
+                </ul>
+            </nav>
+            <div class="btns-nav">
+                <div class="btn-theme" id="change-theme">
+                    <i class="uil uil-moon"></i>
+                </div>
+                <div class="btn-burger">&equiv;</div>
+            </div>
+         
+        </div>
+    </header>
+    <div class="nav-fond"></div>
+    <main class="main">
+        <!-- ACCUEIL -->
+        <section class="accueil" id="accueil">
+            <div class="accueil-container container ">
+                <p class="accueil-text">  Salut, je m'appelle  <b>Liticia TADJER.</b> 
+                    <br>
+                Étudiante en développement Web et Web mobile à Digital Campus.
+                </p>
+      
+                <a href="#a-propos" class="btn-accueil">En savoir plus <i class="uil uil-arrow-down arrow-down"></i> </a>
+            </div>
+        </section>
+        <!-- A propos -->
+        <section class="a-propos section " id="a-propos">
+            <div class="a-propos-container container">
+                <div class="h1-title">
+                    <h1> A propos de moi </h1>
+                </div>
+                <div class="qualites">
+                    <article class="qualite-article">
+                        <div class="qualite-icone">
+                            <img src="src/images/ico1.png" alt="icone">
+                        </div>
+                        <div class="qualite">
+                            <h3>Ponctuelle</h3>
+                        </div>
+                    </article>
+
+                    <article class="qualite-article">
+                        <div class="qualite-icone">
+                            <img src="src/images/ico2.png" alt="icone">
+                        </div>
+                        <div class="qualite">
+                            <h3>Attentive</h3>
+                        </div>
+                    </article>
+                    
+                    <article class="qualite-article">
+                        <div class="qualite-icone">
+                            <img src="src/images/ico3.png" alt="icone">
+                        </div>
+                        <div class="qualite">
+                            <h3>Créative</h3>
+                        </div>
+                    </article>
+                    
+                    <article class="qualite-article">
+                        <div class="qualite-icone">
+                            <img src="src/images/ico4.png" alt="icone">
+                        </div>
+                        <div class="qualite">
+                            <h3>Dynamique</h3>
+                        </div>
+                    </article> 
+                </div>
+                <div class="a-propos-presentation">
+                    <div class="a-propos-photo">
+                        <img src="src/images/ico1.png" alt="">
+                    </div>
+                    <div class="a-propos-text">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam, laudantium assumenda! Eligendi delectus quis doloribus accusantium sunt omnis atque ipsam. Quod molestias in repellat soluta quaerat, et velit itaque odio.</p>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+         <!-- PARCOURS SCOLAIRE -->
+         <section class="parcours-scolaire section" id="a-propos">
+            <div class="h1-title">
+                <h1> Mon parcours scolaire</h1>
+            </div>
+            <div class="parcours-scolaire-container container ">
+                <div class="parcours-data ">
+                    <div> 
+                     <h3 class="parcours-title">L1 informatique</h3>
+                     <span class="parcours-subtitle">Université de bordeaux</span>
+                     <div class="date">2018-2020</div>
+                    </div>
+                    <div>
+                        <span class="rounder"></span>
+                        <span class="line"></span>
+                    </div>
+                </div>
+                <div class="parcours-data">
+                    <div></div>
+                    <div>
+                        <span class="rounder"></span>
+                        <span class="line"></span>
+                    </div>
+                    <div>
+                        <h3 class="parcours-title">L2 informatique</h3>
+                        <span class="parcours-subtitle">Université de bordeaux</span>
+                        <div class="date">2020-2021</div>
+    
+                    </div>
+                           
+             </div>
+                 <div class="parcours-data">
+                     <div>
+                        <h3 class="parcours-title">Développement web</h3>
+                        <span class="parcours-subtitle">Digital Campus</span>
+                        <div class="date">2021-2022</div>  
+                     </div>
+                     
+                     <div>
+                         <span class="rounder"></span>
+                     </div>
+                 </div> 
+               
+            </div>
+        </section>
+        <!-- COMPETENCES -->
+        <section class="competences section" id="competences">
+            <div class="h1-title">
+                <h1> Mes compétences </h1>
+            </div>
+            <div class="competences-container container">
+                <div class="competences-box">
+                    <h3 class="competences-subtitle"><i class="uil uil-brackets-curly icone"></i>
+                        Front-end
+                    </h3>
+                    <span class="competence-name">Html</span>
+                    <span class="competence-name">Css</span>
+                    <span class="competence-name">Javascript</span>
+
+                    <h3 class="competences-subtitle"><i class="uil uil-programming-language icone"></i>
+                        Back-end
+                    </h3>
+                    <span class="competence-name">Php</span>
+                    <span class="competence-name">Python</span>
+                    
+                    <h3 class="competences-subtitle"><i class="uil uil-object-group icone"></i>
+                        Design
+                    </h3>
+                    <span class="competence-name">Photoshop</span>
+                    <span class="competence-name">Illustrator</span>
+                    <span class="competence-name">Adobe xd</span>
+                </div>
+                <div class="competence-img">
+                    <img src="src/images/img1.png" alt="">
+                </div>
+            </div>
+        </section>
+        <!-- PROJETS -->
+        <section class="projets section" id="projets">
+            <div class="h1-title">
+                <h1> Mes projets </h1>
+            </div>
+            <div class="projets-container container swiper ">
+                <div class="swiper-wrapper">
+                    <!-- PROJET 1 -->
+                    <div class="projets-content swiper-slide">
+                        <img src="src/images/img2.png" alt="" class="projets-img"> 
+
+                        <div class="button-projets">
+                            <a href="#projets" class="btn-projet button"> Plus d'infos </a>
+                        </div>
+                    </div>
+                    <!-- PROJET 2 -->
+                    <div class="projets-content swiper-slide">
+                        <img src="src/images/img3.png" alt="" class="projets-img"> 
+
+                        <div class="button-projets">
+                            <a href="#" class="btn-projet button"> Plus d'infos </a>
+                        </div>
+                    </div>
+                    <!-- PROJET 3 -->
+                    <div class="projets-content swiper-slide">
+                        <img src="src/images/img4.png" alt="" class="projets-img"> 
+
+                        <div class="button-projets">
+                            <a href="#" class="btn-projet button"> Plus d'infos </a>
+                        </div>
+                    </div>
+                </div>
+                
+                <!-- ARROWS -->
+                <div class="swiper-button-next">
+                    <i class="uil uil-angle-right-b swiper-projets-icon"></i>
+                </div>
+                <div class="swiper-button-prev">
+                    <i class="uil uil-angle-left-b swiper-projets-icon"></i>
+                </div>
+
+                <div class="swiper-pagination"></div>
+   
+            </div>
+        </section>
+         <!-- CONTACT -->
+         <section class="contact section" id="contact">
+            <div class="h1-title">
+                <h1> Contactez-moi </h1>
+            </div>
+            <div class="contact-container container grid">
+                <div class="contact-infos"> 
+                    <div class="contact-info">
+                        <i class="uil uil-phone contact-icon"></i>
+                        <div> 
+                            <h3 class="contact-title"> Appelez-moi
+                            </h3>
+                            <span class="contact-subtitle">
+                                06 28 35 60 20
+                            </span>
+                        </div>
+                      
+                    </div>
+                    <div class="contact-info">
+                        <i class="uil uil-envelope contact-icon"></i>
+                        <div>   
+                            <h3 class="contact-title"> E-mail
+                            </h3>
+                            <span class="contact-subtitle">
+                                tadjerliticia@gmail.com
+                            </span>
+                        </div>
+                        
+                    </div>
+                </div>
+                
+                    
+                  
+                
+                <form action="" class="contact-form gird js-form-verif" method="post">
+                    <div class="contact-inputs">
+                    <div>
+                        <label for="contact-nom"></label>
+                        <input type="text"  data-min="1" data-max="12" placeholder=" Votre nom" class="contact-input js-verif-text" id="contact-nom" name="nom" required>
+                        <span class="form-error form-error-hidden">Champs à remplir correctement</span>
+                    </div>    
+                    <div>
+                        <label for="contact-email"></label>
+                        <input type="mail"  data-min="8" data-max="255" placeholder="Votre e-mail" class="contact-input js-verif-email" id="contact-email" name="email" required>
+                        <span class="form-error form-error-hidden">Champs à remplir correctement</span>
+                    </div>
+                    <div>
+                        <label for="contact-type"></label>
+                        <select class="contact-input js-verif-select" name="objet" id="contact-type">
+                            <option value="">Choisir</option>
+                            <option value="contact">Demande de contact</option>
+                            <option value="entretien">Proposition d'entretien</option>
+                            <option value="autre">Autre</option>
+                          
+                      </select>
+                        <span class="form-error form-error-hidden">Champs à remplir correctement</span>
+                    </div>   
+                    <div>
+                        <label for="contact-message"></label>
+                        <textarea name="" id="contact-message" cols="0" rows="8" class="contact-input message js-verif-text" placeholder="Votre message"></textarea> 
+                        <span class="form-error form-error-hidden">Champs à remplir correctement</span>
+                    </div>
+                    <div class="form-check">
+                        <label for="contact-rgpd">
+                            <input class="js-verif-rgpd" id="contact-rgpd" type="checkbox" name="rgpd">
+                            J'accepte les conditions d'utilisations.
+                            <span class="form-error form-error-hidden">Case à cocher</span>
+                        </label>
+                    </div>
+
+                   <input type="submit" value="Envoyez" class="button contact-button" disabled>
+                   
+                   <div class="form-error-principal form-error-principal-success">Votre formulaire a été envoyé avec succès.</div>
+  
+                    </div>
+                    
+                   
+                    
+                   
+
+                   
+                </form>
+                
+
+                
+               
+            </div>
+        </section>
+
+    </main>
+    <!-- FOOTER -->
+    <footer class="main-footer">
+        <div class="footer-container grid">
+            <p> &copy; Liticia TADJER </p>
+            <div class="links grid">
+                <a href="#a-propos" class="link">A propos</a>
+                <a href="#projets"class="link">Projets</a>
+                <a href="#contact" class="link">Contact</a>
+            </div>
+            <a href="#">Mentions légales</a>
+        </div>    
+    </footer>
+    <!-- SCROLL UP -->
+    <a href="#" class="scroll-up" id="scroll-up">
+        <i class="uil uil-arrow-up scroll-up-icon"></i>
+    </a>
+    <!-- SWIPER -->
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script> 
+    <!-- JS -->
+    <script src="src/js/script.js"></script>
+    <script src="src/js/form.js"></script> 
+</body>
+</html>
