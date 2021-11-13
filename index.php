f6e5303c10549a960f16bfc4472105f84954109e
Dare Samuel ANYARONKE <dwayne-12@hotmail.fr>
2021-11-13 12:44:48 +0100

    mini router non complet

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
 <?php
 
-echo $_GET["page"];
+//  on recupere notre URL
+$url = $_GET["page"];
+
+if (isset($url) && !empty($url)) {
+
+    // on match les url pour afficher les bonnes pages
+    try {
+        switch ($url) {
+            case "/" :
+            case "home":
+            case "acceuil":
+                echo "page d'acceuil";
+                break;
+            case "contact" :
+                echo "page de contact";
+                break;
+            default : throw new Exception("la page n'existe pas");
+        }
+
+    } catch (Exception $e) {
+        die( $e->getMessage() );
+    }
+} else {
+    // afficher la page d'acceuil
+    echo "afficher acceuil ";
+}
