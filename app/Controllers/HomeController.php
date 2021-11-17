2ee08184e43fa82c2418a8125b2a9213e06388c8
Dare Samuel ANYARONKE <dwayne-12@hotmail.fr>
2021-11-17 19:44:00 +0100

    other

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -10,6 +10,6 @@ class HomeController extends Controller {
      */
     public function home()
     {
-        $this->views("home");
+        $this->views("template/layout");
     }
 }
--- app/Views/template/layout.php
+++ app/Views/template/layout.php
@@ -5,13 +5,81 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css" />
+    <link rel="stylesheet" href="public/css/other.css">
     <title>Document</title>
 </head>
 <body>
 
 
-    <div class="content">
-        <?= $contents; ?>
+    <div class="container">
+
+        <section >
+            <div class="presentation">
+                <h1>Hello world, ici Lettycia</h1>
+                <p>
+                    jeune étudiante passioner par le devellopement web , mobile et l'ux disign, Je serai..... Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet delectus esse quam quas vel? Ad, cupiditate facere illo, iste iure nemo nisi odit officiis perspiciatis quod rem repellat ut vel.
+                </p>
+                <a href="#" class="btn-contact">Contacter moi</a>
+            </div>
+        </section>
+
+        <section >
+
+            <div class="competence">
+                <h2>Ce que je sais faire & apprend</h2>
+                <div class="competence-items">
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-html5"></i></div>
+                        <div class="item-name">html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-css3-alt"></i></div>
+                        <div class="item-name">css3</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-js-square"></i></div>
+                        <div class="item-name">html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-php"></i></div>
+                        <div class="item-name"> html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-python"></i></div>
+                        <div class="item-name">html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-html5"></i></div>
+                        <div class="item-name">html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-css3-alt"></i></div>
+                        <div class="item-name">css3</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-js-square"></i></div>
+                        <div class="item-name">html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-php"></i></div>
+                        <div class="item-name"> html5</div>
+                    </div>
+                    <div class="competence-items-card">
+                        <div class="item"><i class="fab fa-python"></i></div>
+                        <div class="item-name">html5</div>
+                    </div>
+                </div>
+            </div>
+            <div class="competence">
+                <img src="public/images/dev.png" alt="">
+            </div>
+        </section>
+
+        <section >
+            <h1>page one </h1>
+        </section>
+
     </div>
 </body>
 </html>
