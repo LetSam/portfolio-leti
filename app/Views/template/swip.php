00203e685c8d305b63ed1070684593f2ae9ca16f
ltadjer <94218573+ltadjer@users.noreply.github.com>
2021-11-19 22:42:56 +0100

    swip

---
+++ app/Views/template/swip.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .projets {
+            display: flex;
+            align-items: center;
+            width: 100%;
+        }
+        img{
+            min-width: 100%;
+            width: 100%;
+        }
+    </style>
+</head>
+<body>
+    <div class="projects">
+        <img src="public/images/bg2.png" alt="">
+        <img src="public/images/bg2.png" alt="">
+        <img src="public/images/bg2.png" alt="">
+    </div>              
+</body>
+</html>
